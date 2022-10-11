d641d751fda7d8322e083c7eb9847e024a10d4c8
rashidatsalau <rashidat.salau@cloudreach.com>
2022-10-11 17:03:27 +0200

    updating list bucket

--- list_buckets.py
+++ list_buckets.py
@@ -1,4 +1,11 @@
 import boto3
+from datetime import datetime
+
 client = boto3.client("s3")
+
 response = client.list_buckets()
-print(response)
+
+
+for bucket in response["Buckets"]:
+    print(datetime.strftime(bucket["CreationDate"], "%Y-%m-%d %H:%M:%S"),
+        bucket["Name"])
